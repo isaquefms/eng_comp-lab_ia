3603a6407c6dba1f89470024aebda4bf6060f6d0
Isaque Fernando <isaquefernando10@gmail.com>
2020-03-16 18:28:00 +0000

    feat: update tree.py

--- prat_3/tree.py
+++ prat_3/tree.py
@@ -69,3 +69,27 @@ class Tree:
         # retirar o nó da borda
         self.edge.remove(node)
         self.search_a_star(priceless_node, destiny)
+ """       
+    @staticmethod
+    def deep_search(root_node: Node, name: str) -> bool:
+        """
+        Método que realiza a busca em profundidade em uma árvore.
+        :param root_node: Nó raiz da árvore.
+        :param name: Nome do nó que queremos procurar.
+        :return: Sucesso ou falha na busca por esse nó.
+        """
+        if root_node.get_name() == name:
+            return True
+        child_nodes = root_node.get_child_nodes()
+        for child_node in child_nodes:
+            # encontrou
+            print(child_node.get_name())
+            if child_node.get_name() == name:
+                return True
+            # não encontrou na primeira profundidade porém tem filhos
+            elif child_node.get_child_nodes():
+                if Tree.deep_search(child_node, name):
+                    return True
+            # é nó terminal, não sendo o que nós queremos e não tem filho
+        return False
+"""
