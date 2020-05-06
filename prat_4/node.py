989d515314485a0c7784f5648d7fe76194369e96
Isaque Fernando <isaquefernando10@gmail.com>
2020-05-06 17:32:59 -0300

    feat: adição da lógica para verificação da utilidade

--- prat_4/node.py
+++ prat_4/node.py
@@ -73,3 +73,114 @@ class Node:
         Returns: None
         """
         self.utility = utility
+
+    # Methods
+    def state_is_equal(self, other_state: dict) -> bool:
+        """
+        Compara se um passado como parâmetro é igual ao estado armazenado no nó.
+        Args:
+            other_state: Estado a se comparar.
+
+        Returns: Valor booleano informando se o estado é igual ou diferente.
+        """
+        for line in range(1, 4):
+            for column in range(1, 4):
+                key = int('{}{}'.forma(line, column))
+                # caso os elementos de mesma chave sejam diferentes
+                if self.state[key] != other_state[key]:
+                    return False  # já aceleramos o processo retornando falso na operação
+        return True  # se passar por todas as comparações os estados são iguais
+
+    def calculate_utility(self, max_char='x') -> None:
+        """
+        Calcula o valor de utilidade do nó de acordo com o estado e também com a variável de vitória.
+        Args:
+            max_char: Caracter que buscamos como o definidor de vitória. Vamos definir ele como o máximo
+
+        Returns: None
+        """
+        min_char: str = ''
+        # começamos definindo o caracter de minimização
+        if max_char == 'x':
+            min_char = 'o'
+        elif max_char == 'o':
+            min_char = 'x'
+        elif max_char != 'x' and max_char != 'o':  # caso onde houve um erro nas atribuições
+            # voltamos aos valores padrão
+            max_char = 'x'
+            min_char = 'o'
+        # teste das oito condições de vitória ou derrota
+        # primeira condição
+        if self.state[11] == max_char and self.state[12] == max_char and self.state[13] == max_char:
+            self.utility = 1
+            return
+        elif self.state[11] == min_char and self.state[12] == min_char and self.state[13] == min_char:
+            self.utility = -1
+            return
+        # segunda condição
+        elif self.state[21] == max_char and self.state[22] == max_char and self.state[23] == max_char:
+            self.utility = 1
+            return
+        elif self.state[21] == min_char and self.state[22] == min_char and self.state[23] == min_char:
+            self.utility = -1
+            return
+        # terceira condição
+        elif self.state[31] == max_char and self.state[32] == max_char and self.state[33] == max_char:
+            self.utility = 1
+            return
+        elif self.state[31] == min_char and self.state[32] == min_char and self.state[33] == min_char:
+            self.utility = -1
+            return
+        # quarta condição
+        elif self.state[11] == max_char and self.state[21] == max_char and self.state[31] == max_char:
+            self.utility = 1
+            return
+        elif self.state[11] == min_char and self.state[21] == min_char and self.state[31] == min_char:
+            self.utility = -1
+            return
+        # quinta condição
+        elif self.state[12] == max_char and self.state[22] == max_char and self.state[32] == max_char:
+            self.utility = 1
+            return
+        elif self.state[12] == min_char and self.state[22] == min_char and self.state[32] == min_char:
+            self.utility = -1
+            return
+        # sexta condição
+        elif self.state[13] == max_char and self.state[23] == max_char and self.state[33] == max_char:
+            self.utility = 1
+            return
+        elif self.state[13] == min_char and self.state[23] == min_char and self.state[33] == min_char:
+            self.utility = -1
+            return
+        # sétima condição
+        elif self.state[11] == max_char and self.state[22] == max_char and self.state[33] == max_char:
+            self.utility = 1
+            return
+        elif self.state[11] == min_char and self.state[22] == min_char and self.state[33] == min_char:
+            self.utility = -1
+            return
+        # oitava condição
+        elif self.state[31] == max_char and self.state[22] == max_char and self.state[13] == max_char:
+            self.utility = 1
+            return
+        elif self.state[31] == min_char and self.state[22] == min_char and self.state[13] == min_char:
+            self.utility = -1
+            return
+        # se caso nenhuma das condições de vitória forem satisfeitas logos temos 0 utilidade ou um empate
+        else:
+            self.utility = 0
+
+    # Static methods
+    @staticmethod
+    def initial_state() -> dict:
+        """
+        Monta um dicionário com os valores representando o estado inicial. Nesse dicionário, todas as posições são
+        preenchidas com string vazia, inicio de um jogo da velha.
+        Returns: Dicionário com o estado inicial do jogo da velha.
+        """
+        return_dict: dict = {}
+        for line in range(1, 4):
+            for column in range(1, 4):
+                key = int('{}{}'.format(line, column))  # chave do dicionário será a string representante da posição
+                return_dict[key] = ''  # todas as posições do dicionário serão preenchidas com uma string vazia
+        return return_dict
--- prat_4/tree.py
+++ prat_4/tree.py
@@ -16,83 +16,34 @@ class Tree:
             root_node: Nó pai, ou primeiro nível na hierarquia da árvore.
         """
         self.root_node = root_node
+        self.root_node.state = Node.initial_state()
 
-    @staticmethod
-    def initial_state() -> dict:
+    def deep_search(self, root_node: Node, search_state: dict) -> bool:
         """
-        Monta um dicionário com os valores representando o estado inicial. Nesse dicionário, todas as posições são
-        preenchidas com string vazia, inicio de um jogo da velha.
-        Returns: Dicionário com o estado inicial do jogo da velha.
-        """
-        return_dict: dict = {}
-        for line in range(1, 4):
-            for column in range(1, 4):
-                key = int('{}{}'.format(line, column))  # chave do dicionário será a string representante da posição
-                return_dict[key] = ''  # todas as posições do dicionário serão preenchidas com uma string vazia
-        return return_dict
+        Realiza a busca em profundidade na árvore de estados.
+        Args:
+            root_node: Nó inicial de pesquisa.
+            search_state: Estado que desejamos procurar.
 
-    @staticmethod
-    def deep_search(root_node: Node, name: str) -> bool:
-        """
-        Método que realiza a busca em profundidade em uma árvore.
-        :param root_node: Nó raiz da árvore.
-        :param name: Nome do nó que queremos procurar.
-        :return: Sucesso ou falha na busca por esse nó.
+        Returns: True em caso de estado encontrado e False caso contrário.
         """
         # caso base da recursão, se o nó for o nó procurado
-        if root_node.get_name() == name:
+        if root_node.state_is_equal(search_state):
             return True
         # começamos pesquisando todos os nós do nó raiz
         child_nodes = root_node.get_child_nodes()
         for child_node in child_nodes:
-            # exibe o nó visitado atualmente
-            print(child_node.get_name())
             # encontrou
-            if child_node.get_name() == name:
+            if child_node.state_is_equal(search_state):
                 return True
             # não encontrou na profundidade atual porém tem filhos
             elif child_node.get_child_nodes():
                 # chamada recursiva e verificação ao mesmo tempo
-                if Tree.deep_search(child_node, name):
+                if self.deep_search(child_node, search_state):
                     return True
             # é nó terminal, não sendo o que nós queremos e não tem filho, segue para o nó "irmão"
         # se ao final não encontrar nada, podemos retornar false
         return False
-
-    def search_a_star(self, node: Node, destiny: str) -> None:
-        """
-        Algoritmo A* implementado. Ele irá procurar pelos filhos do nó pai e exibir o custo de cada um dos filhos pela
-        função f(n) = g(n) + h(n).
-        :return:
-        Args:
-            node: Nó raiz.
-            destiny: String com o destino que desejamos alcançar.
-
-        Returns: Não há retorno.
-        """
-        # exibição das mensagens
-        print('Estou em: {}'.format(node.get_name()))
-        print('Preço real para se chegar ate aqui: {}'.format(node.get_cost()))
-        print('Distância em linha reta até Bucareste: {}'.format(node.get_heuristic()))
-        print('Preço total: {}'.format(node.get_cost_function()))
-
-        # caso base, o no atual é o nó procurado
-        if node.get_name() == destiny:
-            return
-        # atualizar a borda
-        for child_node in node.get_child_nodes():
-            self.edge.append(child_node)
-        # procura pelo nó mais barato para expandir
-        price: float = math.inf  # nesse caso atribuímos a price um valor infinito
-        priceless_node: Node = None  # futuro nó mais barato
-        # para cada nó na borda
-        for edge_node in self.edge:
-            if edge_node.get_cost_function() < price:
-                priceless_node: Node = edge_node
-                price = edge_node.get_cost_function()
-        # retirar o nó da borda
-        self.edge.remove(node)
-        self.search_a_star(priceless_node, destiny)
 
     def minmax_algorithm(self, node: Node, depth: int, maxmizing_player: bool) -> int:
         """
@@ -106,7 +57,7 @@ class Tree:
         """
         # se caso a profundidade tenha sido alcançada ou o nó não tenha filhos
         if depth == 0 or not node.get_child_nodes():
-            return node.get_heuristic()
+            return node.get_utility()
         # caso o objetivo seja maximizar a jogada
         if maxmizing_player:
             # atribuímos um valor exorbitante para comparação
