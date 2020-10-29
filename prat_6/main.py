092dc8067aaac5ab15ae5cbcd3b3f3fbd99d6ba4
Isaque Fernando <isaquefernando10@gmail.com>
2020-10-29 10:58:55 -0300

    Fechou com 10. Ag pronto
    Falta ajustar algumas coisas para melhor entendimento e montar o readme

--- prat_6/main.py
+++ prat_6/main.py
@@ -1,5 +1,6 @@
 import math
 import random
+import matplotlib.pyplot as plt
 
 from typing import List, Tuple
 
@@ -15,6 +16,24 @@ def funcao_objetivo(x: float, y: float) -> float:
 	"""
 	return math.sin(x) * math.exp(math.pow(1 - math.cos(y), 2)) + \
 		math.cos(y) * math.exp(math.pow(1 - math.sin(x), 2)) + math.pow(x - y, 2)
+
+
+def retorno_geracao(populacao: List[List[float]], tam_populacao: int) -> List[float]:
+	"""Prepara o retorno de uma determinada geração.
+
+	Args:
+		populacao: Individuos de uma determinada geração.
+		tam_populacao: Tamanho da população.
+
+	Returns: Lista com o resultado do melhor, do pior e a média dos resultados.
+	"""
+	# o melhor é o último indivíduo e o pior o primeiro
+	melhor = funcao_objetivo(populacao[-1][0], populacao[-1][1])
+	pior = funcao_objetivo(populacao[0][0], populacao[0][1])
+	soma_total: float = 0
+	for individuo in populacao:
+		soma_total += funcao_objetivo(individuo[0], individuo[1])
+	return [melhor, pior, soma_total/tam_populacao]
 
 
 def cria_populacao(tam_populacao: int) -> List[List[float]]:
@@ -62,16 +81,17 @@ def avalia_populacao(populacao: List[List[float]]) -> List[List[float]]:
 	return populacao_final
 
 
-def seleciona_pai(populacao: List[List[float]], num_sorteado: int) -> List[float]:
+def seleciona_pai(populacao: List[List[float]]) -> List[float]:
 	"""Seleciona dentre os elementos um possível pai de acordo com a 
 	probabilidade.
 
 	Args:
 		populacao: População de elementos com a sua aptidão.
-		num_sorteado: Número sorteado pela roleta.
 
 	Returns: Elemento sorteado.
 	"""
+	# número sorteado
+	num_sorteado = random.randint(1, 100)
 	soma_relativa: int = 0
 	for indice, individuo in enumerate(populacao):
 		# soma relativa é igual ao intervalo da aptidão do indicíduo
@@ -80,7 +100,8 @@ def seleciona_pai(populacao: List[List[float]], num_sorteado: int) -> List[float
 		# podemos retornar o individuo atual
 		if num_sorteado <= soma_relativa:
 			return individuo
-	# caso nenhum número seja retornado anteriormente, retornamos o último
+	# caso nenhum número seja retornado anteriormente, retornamos o último,
+	# no caso o melhor, isso é um certo elitismo
 	return populacao[-1]
 
 
@@ -114,7 +135,7 @@ def cruzamento(pai_1: List[float], pai_2: List[float], taxa_de_cruzamento: float
 		# filho 1 é igual ao pai 1
 		filho_1.append(pai_1[0])
 		filho_1.append(pai_1[1])
-		# filho 2 é igual ao pái 2
+		# filho 2 é igual ao pai 2
 		filho_2.append(pai_2[0])
 		filho_2.append(pai_2[1])
 	return filho_1, filho_2
@@ -133,49 +154,71 @@ def mutacao(filho: List[float], taxa_de_mutacao: float) -> List[float]:
 	filho_mutado: List[float] = []
 	# número aleatório para avaliar se o indivíduo será mutado
 	num_aleat = random.uniform(0, 1)
-	# variável para selecionar qual elemento vamos mutar
-	mutar_x: bool = False
+	# fator de mutação
+	fator_mutacao = random.uniform(-0.2, 0.2)
+	# gene que será mutado
+	gene = random.randint(0, 1)
 	# se for mutado
 	if num_aleat <= taxa_de_mutacao:
-		if mutar_x:
-			# muta o gene x
-			filho_mutado.append(filho[0] + 0.2)
-			filho_mutado.append(filho[1])
-			# na próxima irá mutar y
-			mutar_x = not mutar_x
-		else:
-			# muta o gene y
-			filho_mutado.append(filho[0])
-			filho_mutado.append(filho[1] - 0.3)
-			# na próxima irá mutar x
-			mutar_x = not mutar_x
+		filho[gene] = filho[gene] + fator_mutacao
+		filho_mutado.append(filho[0])
+		filho_mutado.append(filho[1])
 	else:
 		filho_mutado.append(filho[0])
 		filho_mutado.append(filho[1])
 	return filho_mutado
 
 
+def algoritmo_genetico(tam_populacao: int, taxa_de_cruzamento: float, taxa_de_mutacao: float, limite_geracoes: int) -> List[List[float]]:
+	"""Execução do algoritmo genético.
+
+	Args:
+		tam_populacao: Valor que indica o tamanho da população.
+		taxa_de_cruzamento: Valor entre 0 e 1 que representa a probabilidade
+		de se ocorrer o cruzamento.
+		taxa_de_mutacao: Valor entre 0 e 1 que representa a probabilidade de
+		se ocorrer a mutação.
+		limite_geracoes: Limite máximo de gerações.
+
+	Returns: Matriz com o melhor e o pior indivíduo da população.
+	"""
+	# primeira população
+	p = cria_populacao(tam_populacao)
+	g: int = 0  # gerações
+	individuos: List[List[float]] = []  # melhor e pior individuo da pop
+	while g < limite_geracoes:
+		# avaliação da população
+		p_avaliada = avalia_populacao(p)
+		# o melhor individuo está na última posição e o pior na primeira
+		# retirando a aptidão que é retornada no avalia_populacao
+		individuos.append(retorno_geracao(p_avaliada, tam_populacao))
+		# nova geração
+		q: List[List[float]] = []
+		# para cada casal da população
+		for _ in range(tam_populacao // 2):
+			pai_1 = seleciona_pai(p_avaliada)
+			pai_2 = seleciona_pai(p_avaliada)
+			filho_1, filho_2 = cruzamento(pai_1, pai_2, taxa_de_cruzamento)
+			filho_1 = mutacao(filho_1, taxa_de_mutacao)
+			filho_2 = mutacao(filho_2, taxa_de_mutacao)
+			# adiciona os filhos
+			q.append(filho_1)
+			q.append(filho_2)
+		# próxima geração
+		g += 1
+		# nova população é igual a q
+		p = q
+	return individuos
+
+
 def main():
-	p = cria_populacao(3)
-	p_final = avalia_populacao(p)
-	print(p_final)
-	x1 = random.randint(1, 100)
-	x2 = random.randint(1, 100)
-	# x3 = random.randint(1, 100)
-	# x4 = random.randint(1, 100)
-	# x5 = random.randint(1, 100)
-	# x6 = random.randint(1, 100)
-	pai_1 = seleciona_pai(p_final, x1)
-	pai_2 = seleciona_pai(p_final, x2)
-	# print(seleciona_pai(p_final, x3))
-	# print(seleciona_pai(p_final, x4))
-	# print(seleciona_pai(p_final, x5))
-	# print(seleciona_pai(p_final, x6))
-	# print(funcao_objetivo(-4.30, -1.58))
-	# print(funcao_objetivo(2.39, -5.65))
-	# print(funcao_objetivo(6.56, -7.34))
-	print(cruzamento(pai_1, pai_2, 1.0))
-	pass
+	individuos = algoritmo_genetico(20, 0.7, 0.1, 20)
+	geracoes = range(1, 20+1)
+	melhores = [individuo[0] for individuo in individuos]
+	piores = [individuo[1] for individuo in individuos]
+	medias = [individuo[2] for individuo in individuos]
+	plt.plot(geracoes, melhores, 'g--', geracoes, medias, 'b--', geracoes, piores, 'r--')
+	plt.show()
 
 
 main()
